77a3e588201a86e4fb491f0ff8d91179ba130217
Roshi28 <kollasairoshini@gmail.com>
2025-11-03 12:38:55 +0530

    changes made to hmm.py

--- hmm.py
+++ hmm.py
@@ -2,8 +2,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import re
 from hmmlearn import hmm
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
 
 data_corpus = pd.read_csv("Data/Data/corpus.txt", sep="\t", header=None)
 data_test = pd.read_csv("Data/Data/test.txt", sep="\t", header=None)
@@ -16,7 +18,12 @@ encoder = LabelEncoder()
 encoder.fit(list(letters))
 
 #converting all words in corpus to numeric sequences
-sequences = [encoder.transform(list(word[0].lower())) for word in data_corpus.values]
+sequences = []
+for word in data_corpus.values:
+    cleaned_word = re.sub(r'[^a-z]', '', word[0].lower())  # keep only a–z
+    if cleaned_word:  # skip empty strings
+        seq = encoder.transform(list(cleaned_word))
+        sequences.append(seq)
 X = np.concatenate(sequences)
 lengths = [len(seq) for seq in sequences]
 
@@ -29,13 +36,33 @@ transmat = np.array([
 ])
 
 emissionprob = np.array([
-    [0.30, 0.70],   # Playing
-    [1.00, 0.00],   # Won
-    [0.00, 1.00],   # Lost
+    [0.30, 0.70],
+    [1.00, 0.00],
+    [0.00, 1.00],
 ])
 
 model = hmm.MultinomialHMM(n_components=len(state_space), n_iter=100, tol=0.01, random_state=42)
 model.startprob_ = startprob
 model.transmat_ = transmat
-model.emmissionprob_ = emissionprob
+model.emissionprob_ = emissionprob
 model.fit(X.reshape(-1, 1), lengths)
+
+test_sequences = [encoder.transform(list(word[0].lower())) for word in data_test.values]
+X_test = np.concatenate(test_sequences)
+lengths_test = [len(seq) for seq in test_sequences]
+
+logprob, hidden_states = model.decode(X_test.reshape(-1, 1), algorithm = "viterbi")
+
+predicted_letters = []
+true_letters = []
+
+for i in range(len(hidden_states) - 1):
+    state = hidden_states[i]
+    next_obs = X_test[i+1]
+    emission_probs = model.emissionprob_[state]
+    predicted_letter = np.argmax(emission_probs)
+    predicted_letters.append(predicted_letter)
+    true_letters.append(next_obs)
+
+hmm_accuracy = accuracy_score(true_letters, predicted_letters)
+print(hmm_accuracy)
